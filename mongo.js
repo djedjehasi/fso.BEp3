b6e8f8f455080737d4385e503d394d8aed112ba3
Yosef Gehasy <djedjehasi@gmail.com>
2025-01-31 12:54:11 +1000

    now when just connecting to mongo it will print the whole phonebook stored atm

--- mongo.js
+++ mongo.js
@@ -1,34 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
-const url =
-  `mongodb+srv://djedjehasi:${password}@cluster0.9mxit.mongodb.net/phoneBookApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://djedjehasi:${password}@cluster0.9mxit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false);
 
 mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
 
-const personsSchema = new mongoose.Schema({
-  name: String,
-  numnber: String,
-})
+    const personSchema = new mongoose.Schema({
+      name: String,
+      number: String, // Corrected from 'numnber'
+    });
 
-const Persons = mongoose.model('Persons', personsSchema)
+    const Person = mongoose.model('Person', personSchema);
 
-const persons = new Persons({
-    name: name,
-    numnber: number,
-})
+    if (name && number) {
+      // Save a new person if name and number are provided
+      const person = new Person({
+        name: name,
+        number: number,
+      });
 
-persons.save().then(result => {
-  console.log(`added ${name} number ${number} to phonebook`)
-  mongoose.connection.close()
-})
+      return person.save()
+        .then(() => {
+          console.log(`added ${name} number ${number} to phonebook`);
+        });
+    } else {
+      // Fetch all person if no name and number are provided
+      return Person.find({}).then(person => {
+        console.log('Phonebook:');
+        person.forEach(person => {
+          console.log(`${person.name} ${person.number}`);
+        });
+      });
+    }
+  })
+  .then(() => {
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    mongoose.connection.close();
+  });
