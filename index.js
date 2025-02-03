c93d4dcc5a2ce7f430ac75b44738ab3bb792108a
Yosef Gehasy <djedjehasi@gmail.com>
2025-02-03 15:04:01 +1000

    deleting persons from MnogoDB

--- index.js
+++ index.js
@@ -34,19 +34,14 @@ app.get('/api/persons/:id', (request, response) => {
   response.json(person)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(erorr => next(error))
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)  
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
