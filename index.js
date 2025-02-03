870294bfdd92e16a8a3efa435de9c06b37208bff
Yosef Gehasy <djedjehasi@gmail.com>
2025-02-03 13:09:38 +1000

    fetching data from MongoDB

--- index.js
+++ index.js
@@ -15,37 +15,16 @@ app.use(morgan('customTiny'))
 app.use(cors())
 
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-let info = `<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`
+let info = `<p>Phonebook has info for ${Person.length} people</p> <p>${Date()}</p>`
 
 app.get('/info', (request, response) => {
     response.send(info)
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
