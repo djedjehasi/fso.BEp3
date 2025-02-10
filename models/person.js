8e20688556dd88da48ac9d4f61f5bd5e28d8ff4a
Yosef Gehasy <djedjehasi@gmail.com>
2025-02-10 12:11:22 +1000

    adding number costum validator

--- models/person.js
+++ models/person.js
@@ -17,9 +17,23 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
-        minLength: 3,
+        validate: {
+            validator: (num) => {
+                if (num.length < 8)  return false
+                if(num[2] !== '-' && num[3] !== '-') return false
+                const parts = num.split('-')
+                const [first, secound] = parts
+                if (Number(first) && Number(secound)) {
+                    return true
+                } else {
+                    return false
+                } 
+            },
+            message: "Phone Number Invalid - must contain 8 digits, 1 separator (-) after second or third digit and contain only numbers."
+        },
         required: true
     },
+        
     number: {
         type: String,
         required: true
